6b0e1b6c74faddead7084e349589637f580f1387
sohailinsta <sohailkhaninsta@gmail.com>
2022-03-28 22:30:09 +0530

    commiting

--- src/components/Form.js
+++ src/components/Form.js
@@ -105,7 +105,7 @@ export default function Form() {
 
 <div class="modal fade container" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content ">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
